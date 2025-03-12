f03dce97ee0b2fbea3bd984b43db606154dbdb8d
botbrunomar <botbrunomar@outlook.com>
2025-03-12 16:49:21 +0700

    commit

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import random
-
+import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -10,27 +10,20 @@ load_dotenv(override=True)
 # Initialize the bot
 bot = commands.Bot(
     command_prefix='PREFIX',
-    intents = discord.Intents.all()
+    intents=discord.Intents.all()
 )
 
 # Define the on_ready event
 @bot.event
 async def on_ready():
+    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
     win_numbers = {11, 22, 33, 44, 55, 66, 77, 88, 99}
-    if random.randint(1, 100) in win_numbers:
-        print('You Win!')
-    else:
-        print('You Lose!')
-
-# Define a sample command
-@bot.command(name='play')
-async def play(ctx):
-    win_numbers = {11, 22, 33, 44, 55, 66, 77, 88, 99}
-    if random.randint(1, 100) in win_numbers:
-        await ctx.send('You Win!')
-    else:
-        await ctx.send('You Lose!')
+    result = 'You Win!' if random.randint(1, 100) in win_numbers else 'You Lose!'
+    print(result)
 
 # Run the bot
 if __name__ == '__main__':
-    bot.run(os.getenv('TOKEN'))
+    try:
+        bot.run(os.getenv('TOKEN'))
+    except Exception as e:
+        print(f'Error occurred: {e}')

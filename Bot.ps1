4a47b85e80d128342cabcac2d02caa3b8389df37
botbrunomar <botbrunomar@outlook.com>
2025-03-12 13:25:02 +0700

    Add VS Code remote tunnel and update README; include requirements.txt

---
+++ Bot.ps1
@@ -0,0 +1,5 @@
+# Start a VS Code remote tunnel
+vscode tunnel &
+
+# Run the Python script using the virtual environment's Python interpreter
+C:/Users/botbr/Documents/GitHub/mybot/.venv/Scripts/python.exe c:/Users/botbr/Documents/GitHub/mybot/bot.py
